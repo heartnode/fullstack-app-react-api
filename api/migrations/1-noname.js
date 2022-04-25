8e145d760b9b4187953e462fd061d17d37f824a4
heartnode <weinianhe@gmail.com>
2022-04-25 15:20:55 -0700

    updated packages for deployment

---
+++ api/migrations/1-noname.js
@@ -0,0 +1,194 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "Users", deps: []
+ * createTable "Courses", deps: [Users]
+ *
+ **/
+
+var info = {
+    "revision": 1,
+    "name": "noname",
+    "created": "2022-04-25T22:17:56.768Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+        fn: "createTable",
+        params: [
+            "Users",
+            {
+                "id": {
+                    "type": Sequelize.INTEGER,
+                    "field": "id",
+                    "autoIncrement": true,
+                    "primaryKey": true
+                },
+                "firstName": {
+                    "type": Sequelize.STRING,
+                    "field": "firstName",
+                    "validate": {
+                        "notNull": {
+                            "msg": "First name is required."
+                        },
+                        "notEmpty": {
+                            "msg": "First name is required."
+                        }
+                    },
+                    "allowNull": false
+                },
+                "lastName": {
+                    "type": Sequelize.STRING,
+                    "field": "lastName",
+                    "validate": {
+                        "notNull": {
+                            "msg": "Last name is required."
+                        },
+                        "notEmpty": {
+                            "msg": "Last name is required."
+                        }
+                    },
+                    "allowNull": false
+                },
+                "emailAddress": {
+                    "type": Sequelize.STRING,
+                    "field": "emailAddress",
+                    "validate": {
+                        "notNull": {
+                            "msg": "A valid email address is required."
+                        },
+                        "notEmpty": {
+                            "msg": "A valid email address is required."
+                        },
+                        "isEmail": {
+                            "msg": "A valid email address is required."
+                        }
+                    },
+                    "unique": {
+                        "msg": "The email you entered already exists."
+                    },
+                    "allowNull": false
+                },
+                "password": {
+                    "type": Sequelize.STRING,
+                    "field": "password",
+                    "validate": {
+                        "notNull": {
+                            "msg": "A password is required."
+                        }
+                    },
+                    "allowNull": false
+                },
+                "createdAt": {
+                    "type": Sequelize.DATE,
+                    "field": "createdAt",
+                    "allowNull": false
+                },
+                "updatedAt": {
+                    "type": Sequelize.DATE,
+                    "field": "updatedAt",
+                    "allowNull": false
+                }
+            },
+            {}
+        ]
+    },
+    {
+        fn: "createTable",
+        params: [
+            "Courses",
+            {
+                "id": {
+                    "type": Sequelize.INTEGER,
+                    "field": "id",
+                    "autoIncrement": true,
+                    "primaryKey": true,
+                    "allowNull": false
+                },
+                "title": {
+                    "type": Sequelize.STRING,
+                    "field": "title",
+                    "validate": {
+                        "notNull": {
+                            "msg": "Title is required."
+                        },
+                        "notEmpty": {
+                            "msg": "Title is required."
+                        }
+                    },
+                    "allowNull": false
+                },
+                "description": {
+                    "type": Sequelize.TEXT,
+                    "field": "description",
+                    "validate": {
+                        "notNull": {
+                            "msg": "Description is required."
+                        },
+                        "notEmpty": {
+                            "msg": "Description is required."
+                        }
+                    },
+                    "allowNull": false
+                },
+                "estimatedTime": {
+                    "type": Sequelize.STRING,
+                    "field": "estimatedTime"
+                },
+                "materialsNeeded": {
+                    "type": Sequelize.STRING,
+                    "field": "materialsNeeded"
+                },
+                "createdAt": {
+                    "type": Sequelize.DATE,
+                    "field": "createdAt",
+                    "allowNull": false
+                },
+                "updatedAt": {
+                    "type": Sequelize.DATE,
+                    "field": "updatedAt",
+                    "allowNull": false
+                },
+                "userId": {
+                    "type": Sequelize.INTEGER,
+                    "field": "userId",
+                    "onUpdate": "cascade",
+                    "onDelete": "cascade",
+                    "references": {
+                        "model": "Users",
+                        "key": "id"
+                    },
+                    "allowNull": true
+                }
+            },
+            {}
+        ]
+    }
+];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
