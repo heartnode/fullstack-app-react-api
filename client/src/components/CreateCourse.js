6588c8da8960f82bdc81993481112099a8fffab7
heartnode <weinianhe@gmail.com>
2022-04-25 14:05:02 -0700

    update create course with comment

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -4,18 +4,25 @@ import { Context } from '../Context';
 import Data from '../Data';
 
 const CreateCourse = (props) => {
+    //Setup and initialize states for title, description, estimatedTime, materialsNeeded, errors
     const [ title, setTitle ] = useState('');
     const [ description, setDescription ] = useState('');
     const [ estimatedTime, setEstimatedTime] = useState('');
     const [ materialsNeeded, setMaterialsNeeded] = useState('');
     const [ errors, setErrors ] = useState([]);
+
+    //Use global Context 
     const context = useContext(Context);
+
+    //Gets the credential from context and author information
     const { credential } = context;
     const { firstName, lastName } = context.authenticatedUser;
    
+    //Update the states of each form field
     const  change = (event) => {
         const name = event.target.name;
         const value = event.target.value;
+        // Sets the states base on the field name
         if (name === "courseTitle"){
             setTitle(value);
         } else if (name === "courseDescription"){
@@ -28,22 +35,30 @@ const CreateCourse = (props) => {
             alert("ERROR: Value not watched in create course");
         }
       };
+
+    // Redirect user back to index page when Cancel button is clicked
     const cancel = () => {
         props.history.push('/');
     }
+
+    // On Submit button is clicked
     const submit = () => {
+        //Create new course by calling Rest API
         const data = new Data();
         const course = { title, description, estimatedTime, materialsNeeded, userId:context.authenticatedUser.id};
         data.createCourse(course,credential)
           .then( errors => {
+            // If errors array is set update the errors state
             if (errors.length){
               setErrors(errors);
             } else {
+              // If no errors is a success for creating new course redirect to listing page  
               props.history.push('/');
               console.log(`Successfully created new course`);   
             }
           })
           .catch( err=>{
+            // If returns error redirect to unhandled error page 
             console.log(err);
             props.history.push('/error');
           });
