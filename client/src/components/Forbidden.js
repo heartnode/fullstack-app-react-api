00485da87050ae6437fc43a963e3f64b225bed43
heartnode <weinianhe@gmail.com>
2022-04-25 11:40:58 -0700

    updated forbidden

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,7 +1,12 @@
 import React from 'react';
 const Forbidden = () => {
     return (
-        <p>Forbidden</p>
+        <main>
+        <div className="wrap">
+            <h2>Forbidden</h2>
+            <p>Oh oh! You can't access this page.</p>
+        </div>
+    </main>
     );
 }
 
